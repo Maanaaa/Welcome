a4445c0aefb74f0c38aa70f8a101c3d6b8268afb
Théo <manya.th@icloud.com>
2025-06-21 11:48:41 +0200

    Test de déploiement 7

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Welcome to MMI !</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+        <h1>Test de déploiement 7</h1>
+    </body>
+</html>
