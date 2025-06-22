39a6bf1272fee5e930f1c6f450015b49f14dff12
Théo <manya.th@icloud.com>
2025-06-22 15:19:37 +0200

    front: Modifications du responsive

--- index.php
+++ index.php
@@ -4,32 +4,35 @@
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/index.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css2?family=Obviously&family=Chill+Script&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
     </head>
     <body>
-        <img src="assets/ge/peace.svg" class="traversing-image">
-        <header>
-            <span class="large-icon">🎓</span>
-            <h1>Welcome !</h1>
-            <p>Découvre <span>ton parrain mystère</span> et commence ton aventure à l'IUT ! ✨</p>
-            <h2>🌟 🎉 🚀</h2>
-        </header>
-        <main>
-            <div>
-                <form action="#">
-                    <label for="welcomeCode">Entre ton code secret 🔐</label>
-                    <input type="text" maxlength="8" placeholder="EFDHTGF4">
-                    <button>Commencer l'aventure ! 🚀</button>
-                </form>
+        <div>
+            <img src="assets/ge/peace.svg" class="traversing-image">
+            <header>
+                <span class="large-icon">🎓</span>
+                <h1>Welcome !</h1>
+                <p>Découvre <span>ton parrain mystère</span> et commence ton aventure à l'IUT ! ✨</p>
+                <h2>🌟 🎉 🚀</h2>
+            </header>
+            <main>
                 <div>
-                    <p>Ton code te donne accès
-                    à un chat anonyme avec ton parrain/marraine</p>
-                    <p class="welcomeCodeLosted"><a href="">J'ai perdu mon code d'accès 😔</a></p>
+                    <form action="#">
+                        <label for="welcomeCode">Entre ton code secret 🔐</label>
+                        <input type="text" maxlength="8" placeholder="EFDHTGF4">
+                        <button>Commencer l'aventure ! 🚀</button>
+                    </form>
+                    <div>
+                        <p>Ton code te donne accès
+                        à un chat anonyme avec ton parrain/marraine</p>
+                        <p class="welcomeCodeLosted"><a href="">J'ai perdu mon code d'accès 😔</a></p>
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </div>
         <footer>
 
         </footer>
