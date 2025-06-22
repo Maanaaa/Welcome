a14131090f4d42c9f62333e15d37b1b15c915bb6
Théo <manya.th@icloud.com>
2025-06-22 19:45:09 +0200

    front: Modifications du responsive, flexbox

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
     </head>
     <body>
-        <div>
+        <div class="flex">
             <img src="assets/ge/peace.svg" class="traversing-image">
             <header>
                 <span class="large-icon">🎓</span>
