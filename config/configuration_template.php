ed15ba99a652137f9cd0d879c872907f7ab07e10
Théo <manya.th@icloud.com>
2025-06-23 12:52:57 +0200

    Modification de la template configuration

--- config/configuration_template.php
+++ config/configuration_template.php
@@ -1,25 +1,18 @@
 <?php
-require_once ($_SERVER['DOCUMENT_ROOT'] . '/includes/load_env.php');
-loadEnv($_SERVER['DOCUMENT_ROOT'] . '/.env');
-
 $serverAdress = $_SERVER['SERVER_ADDR'];
 
 // Connexion à la db Docker
-if ($serverAdress != "46.202.172.172") {
+if ($serverAdress != "IP serveur 46....") {
     $host = 'db';
-    $dbname = getenv('DEV_DB_NAME');
-    $user = getenv('DEV_DB_USER');
-    $pass = getenv('DEV_DB_PASSWORD');
-    echo $host;
-    echo $dbname;
-    echo $user;
-    echo $pass;
+    $dbname = "";
+    $user = "";
+    $pass = "";
 }
 // Connexion à Hostinger
 else {
     $host = '127.0.0.1';
-    $dbname = getenv('HOST_DB_NAME');
-    $user = getenv('HOST_DB_USER');
-    $pass = getenv('HOST_DB_PASSWORD');
+    $dbname = "";
+    $user = "";
+    $pass = "";
 }
 
